421f67aac24c4d0ecba11a8d727f8ec7fd5cbef9
1aravi <aravindvasamsetti@gmail.com>
2022-07-18 22:53:41 +0530

    Client side with React

---
+++ src/auth/Github.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import LoginGithub from 'react-login-github';
+
+
+import axios from 'axios';
+
+const Github = ({informParent = f => f}) => {
+    
+    const responseGithub = response => {
+        console.log(response);
+        axios({
+            method: 'POST',
+            url: `${process.env.REACT_APP_API}/github-login`,
+            data: { clientID: response.clientID, accessToken: response.accessToken }
+        })
+
+        .then(response => {
+            console.log('GITHUB SIGNIN SUCCESS', response);
+            // inform parent component
+            informParent(response);
+            
+        })
+        .catch(error => {
+            console.log('GITHUB SIGNIN ERROR', error.response);
+        });
+
+}; 
+
+return (
+    <LoginGithub clientId="362836bcd59b53ccc1ef"
+    onSuccess={responseGithub}
+    onFailure={responseGithub}
+    className={"btn btn-dark btn-lg btn-block"}
+    buttonText={"Login with Github"}
+    
+    
+    />
+);
+};
+
+
+
+export default Github;
+
+
+
+
+
+
+
+
+
+
